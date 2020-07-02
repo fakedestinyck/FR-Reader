13cec54226421966d443ba0aa6968591d567c37e
Caspar Qian <fakedestinyck@gmail.com>
2020-07-03 02:58:17 +0800

    deploy

---
+++ js/precache-manifest.7640f3663a269022a2b1a8928d7ef349.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44195529ff032f8425fa",
+    "url": "/css/app.920cea8a.css"
+  },
+  {
+    "revision": "bb2bde80095b928b930d",
+    "url": "/css/chunk-62cde5b7.d82abbef.css"
+  },
+  {
+    "revision": "182921dd3318dac5c55f",
+    "url": "/css/chunk-vendors.2ac5db4b.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "6d202ab27b16188a953bd297fc9a181e",
+    "url": "/img/logo.6d202ab2.png"
+  },
+  {
+    "revision": "3e479c49f54c7bc82f702c4558f8d8a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65481d3bd74809fb69fe",
+    "url": "/js/about.77e0860d.js"
+  },
+  {
+    "revision": "44195529ff032f8425fa",
+    "url": "/js/app.bb140e55.js"
+  },
+  {
+    "revision": "bb2bde80095b928b930d",
+    "url": "/js/chunk-62cde5b7.6542446b.js"
+  },
+  {
+    "revision": "182921dd3318dac5c55f",
+    "url": "/js/chunk-vendors.5e6a1a75.js"
+  },
+  {
+    "revision": "1a39f9af06c8e9eb57eec6577efb5a5f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
